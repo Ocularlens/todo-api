c831b26a911853806eddd667ad5878532c2ef9ef
Ocularlens <aldous.javier0515@gmail.com>
2023-08-11 11:07:41 +0800

    enhancement: index files logic

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
-const ListModel = require("./list.model");
+const _ = require('lodash');
+const fs = require('fs');
 const { asValue } = require("awilix");
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const {
   MYSQL_DATABASE,
   MYSQL_HOST,
@@ -9,6 +10,21 @@ const {
   MYSQL_PORT,
   NODE_ENV,
 } = require("../config/config");
+
+const loadModels = (sequelize) => {
+  const files = fs.readdirSync(__dirname);
+  const selectedModels = {};
+
+  files.forEach(file => {
+    if (file === 'index.js') return;
+
+    const model = require(`./${file}`);
+    const modelName = _.capitalize(file.split('.')[0])+_.capitalize(file.split('.')[1]);
+    selectedModels[modelName] = asValue(model(sequelize, DataTypes));
+  });
+
+  return selectedModels;
+}
 
 module.exports = () => {
   try {
@@ -23,9 +39,8 @@ module.exports = () => {
     });
 
     // init models
-    const models = {
-      ListModel: asValue(ListModel(dbConnection)),
-    };
+    const models = loadModels(dbConnection);
+
     return { models, dbConnection: asValue(dbConnection) };
   } catch (error) {
     console.log(error);

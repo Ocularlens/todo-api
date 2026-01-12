0f101fc4a783cd109a11df24b1e0e2d51800841e
Aldous Javier <aldous.javier0515@gmail.com>
2026-01-12 09:46:43 +0800

    update yml

--- spec/repositories.spec.js
+++ spec/repositories.spec.js
@@ -2,14 +2,6 @@ const { setUp, container } = require("../src/container");
 
 describe("Repositories", function () {
   beforeAll(async () => {
-    console.log({
-      MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-      MYSQL_USER: process.env.MYSQL_USER,
-      MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
-      MYSQL_HOST: process.env.MYSQL_HOST,
-      MYSQL_PORT: process.env.MYSQL_PORT,
-    })
-
     setUp();
     const database = container.resolve("database");
     await database.sync();
